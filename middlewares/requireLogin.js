7e7f8ac506eb46543c7d8689f45977b107264ee4
swetha.gopinath@texple.com <swethagnthn@gmail.com>
2022-02-24 11:29:49 +0530

    tested apis completed

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const mongoose = require('mongoose')
-const {User} = require("../model/user");
+const { User } = require("../model/user");
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
@@ -18,8 +18,13 @@ module.exports = (req, res, next) => {
         const { _id } = payload;
         User.findById(_id)
             .then(userdata => {
-                req.user = userdata;
-                next();
+                if (userdata) {
+                    req.user = userdata;
+                    next();
+                } else {
+                    return res.status(401).json({ error: "user doesnt exist" });
+                }
+
             })
     })
 
--- routes/user.js
+++ routes/user.js
@@ -1,49 +1,49 @@
 const express = require("express");
-const router  = express.Router();
-const {cloudinary} = require("../utils/cloudinary");
+const router = express.Router();
+const { cloudinary } = require("../utils/cloudinary");
 const upload = require("../utils/multer");
-const {User} = require("../model/user");
-const bcryptjs   = require("bcryptjs");
-const jwt      = require("jsonwebtoken");
+const { User } = require("../model/user");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const requireLogin = require("../middlewares/requireLogin");
-const {File} = require("../model/files");
+const { File } = require("../model/files");
 const shortid = require("shortid");
-const {emailSchema, uploadSchema} = require("../utils/validation");
-const {AUTO} = require("../constant/common")
+const { emailSchema, uploadSchema } = require("../utils/validation");
+const { AUTO } = require("../constant/common")
 
 //  Register User
 //  Params - email, password
 //  Return - message
 router.post("/signup", (req, res) => {
 
-    const params =  {email, password} = req.body;
+    const params = { email, password } = req.body;
 
     const loginValidation = emailSchema.validate(params);
     const { error } = loginValidation;
     if (error) {
-        return res.status(400).json({ message: "Invalid email/password", data: {format: ["password min 2 and max 10", "invalid email format"]}});
+        return res.status(400).json({ message: "Invalid email/password", data: { format: ["password min 2 and max 10", "invalid email format"] } });
     }
 
     User
-    .findOne({email})
-    .then(savedUser => {
-        if(savedUser) return res.status(400).json({error: "User already exists"});
+        .findOne({ email })
+        .then(savedUser => {
+            if (savedUser) return res.status(400).json({ error: "User already exists" });
 
-        bcryptjs.hash(password, 12)
-        .then(hashedPassword => {
-            const user = new User({
-                email,
-                password: hashedPassword,
-            })
-            user.save()
-            .then(user => {
-                res.status(201).json({message: "Created successfully"});
-            })
-            .catch(err => console.log(err))
+            bcryptjs.hash(password, 12)
+                .then(hashedPassword => {
+                    const user = new User({
+                        email,
+                        password: hashedPassword,
+                    })
+                    user.save()
+                        .then(user => {
+                            res.status(201).json({ message: "Created successfully" });
+                        })
+                        .catch(err => console.log(err))
+                })
         })
-    })
-    .catch(err => console.log(err))
-        
+        .catch(err => console.log(err))
+
 });
 
 // LogIn User
@@ -51,67 +51,67 @@ router.post("/signup", (req, res) => {
 //  Return - token, message
 
 router.post("/signin", (req, res) => {
-    const params =  {email, password} = req.body;
+    const params = { email, password } = req.body;
 
     const loginValidation = emailSchema.validate(params);
     const { error } = loginValidation;
     if (error) {
-        return res.status(400).json({ message: "Invalid email/password", data: {format: ["password min 2 and max 10", "invalid email format"]} });
+        return res.status(400).json({ message: "Invalid email/password", data: { format: ["password min 2 and max 10", "invalid email format"] } });
     }
 
     User
-    .findOne({email})
-    .then(savedUser => {
-        if(!savedUser) return res.status(400).json({error: "Invalid email/password"});
-        bcryptjs.compare(password, savedUser.password)
-        .then(doMatch => {
-            if(!doMatch) res.status(400).json({error: "Invalid email/password"}); 
-            
-                let token = jwt.sign({_id: savedUser._id}, process.env.JWT_SECRET, {
-                    expiresIn: process.env.JWT_EXPIRATION_TIME
-                });
-                token = "Bearer " + token;
-                return res.status(200).json({token, message: "Logged in successfully"});
+        .findOne({ email })
+        .then(savedUser => {
+            if (!savedUser) return res.status(400).json({ error: "Invalid email/password" });
+            bcryptjs.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (!doMatch) res.status(400).json({ error: "Invalid email/password" });
+
+                    let token = jwt.sign({ _id: savedUser._id }, process.env.JWT_SECRET, {
+                        expiresIn: process.env.JWT_EXPIRATION_TIME
+                    });
+                    token = "Bearer " + token;
+                    return res.status(200).json({ token, message: "Logged in successfully" });
+                })
+                .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
-    })
-    .catch(err => console.log(err));
 })
 
 // LogIn User
 //  Params - email, password
 //  Return - token, message
 router.post("/upload", requireLogin, upload.single("image"), async (req, res) => {
-    
-    const params = {title, description} = req.query;
+
+    const params = { title, description } = req.query;
 
     const loginValidation = uploadSchema.validate(params);
     const { error } = loginValidation;
     if (error) {
         return res.status(400).json({ error: "Incomplete Fields" });
     }
-    
-    if(!req.file) res.status(400).json({error: "Invalid file path", data: "only image/jpg,image/jpeg,image/png,video/mp4"}); 
 
-    try{
+    if (!req.file) res.status(400).json({ error: "Invalid file path", data: "only image/jpg,image/jpeg,image/png,video/mp4" });
+
+    try {
         // quality and resource type will figure out file type and compress it for storage.
-        const cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, {resource_type: AUTO, quality: AUTO});
+        const cloudinaryResponse = await cloudinary.uploader.upload(req.file.path, { resource_type: AUTO, quality: AUTO });
         const cloudinary_id = cloudinaryResponse.secure_url;
         const file = new File({
-            file_name:req.file.originalname,
+            file_name: req.file.originalname,
             title,
             description,
             cloudinary_id,
             user_id: req.user._id,
         });
         file.save()
-        .then(() => {
-            return res.status(201).json({message: "Created successfully"});
-        }) 
-        .catch(err => res.status(400).json({message: err}))
+            .then(() => {
+                return res.status(201).json({ message: "Created successfully" });
+            })
+            .catch(err => res.status(400).json({ message: err }))
     }
-    catch(err){
-        res.status(400).json({message: err})
+    catch (err) {
+        res.status(400).json({ message: err })
     }
 });
 
@@ -121,11 +121,11 @@ router.post("/upload", requireLogin, upload.single("image"), async (req, res) =>
 router.get("/files", requireLogin, async (req, res) => {
 
     File
-    .find({user_id: req.user._id})
-    .then(file => {
-        res.status(200).json({ message: "Retrived successfully", data: file });
-    })
-    .catch(err => console.log(err))
+        .find({ user_id: req.user._id })
+        .then(file => {
+            res.status(200).json({ message: "Retrived successfully", data: file });
+        })
+        .catch(err => res.status(400).json({ error: err }));
 
 });
 
@@ -135,31 +135,31 @@ router.get("/files", requireLogin, async (req, res) => {
 router.delete("/file/:fileId", requireLogin, (req, res) => {
 
     File
-    .findById({_id:req.params.fileId})
-    .then(file => {
-        if(file.user_id.toString() == req.user._id.toString()){
-            file.remove()
-            .then(() => {
-                res.status(200).json({ message: "Deleted successfully" })
-            })
-            .catch(err => {
-                return res.status(400).json({ error: err });
-            })
-        }else{
-            return res.status(400).json({ error: "File dosent exist" });
-        }    
-    })
-    .catch(() => {
-        return res.status(400).json({ error: "File doesnt exist" });
-    })
+        .findById({ _id: req.params.fileId })
+        .then(file => {
+            if (file.user_id.toString() == req.user._id.toString()) {
+                file.remove()
+                    .then(() => {
+                        res.status(200).json({ message: "Deleted successfully" })
+                    })
+                    .catch(err => {
+                        return res.status(400).json({ error: err });
+                    })
+            } else {
+                return res.status(400).json({ error: "File dosent exist" });
+            }
+        })
+        .catch(() => {
+            return res.status(400).json({ error: "File doesnt exist" });
+        })
 })
 
 // Edit file by a  User
 //  Return - message, file
 
 router.put("/file/:fileId", requireLogin, upload.single("image"), (req, res) => {
-    
-    const params = {title, description} = req.query;
+
+    const params = { title, description } = req.query;
 
     const loginValidation = uploadSchema.validate(params);
     const { error } = loginValidation;
@@ -167,44 +167,50 @@ router.put("/file/:fileId", requireLogin, upload.single("image"), (req, res) => 
         return res.status(400).json({ error: "Incomplete Fields" });
     }
 
-    if(!req.file) res.status(400).json({error: "Invalid file path", data: "only image/jpg,image/jpeg,image/png,video/mp4"}); 
+    if (!req.file) res.status(400).json({ error: "Invalid file path", data: "only image/jpg,image/jpeg,image/png,video/mp4" });
 
     File
-    .findById({_id:req.params.fileId})
-    .then(async file => {
-        if(req.file.originalname != file.file_name){
-            let result = await cloudinary.uploader.upload(req.file.path, {resource_type: AUTO, quality: AUTO});
-            file.cloudinary_id = result.secure_url;
-            file.file_name = req.file.originalname;
-        }
-        file.title = title;
-        file.description = description;
-        file.save()     
-        .then(result => {   
-            if (result) {
-                return res.status(200).json({message:"Successfully edited", result});
-            } else {
-                return res.status(400).json({ error: "err" });
+        .findById({ _id: req.params.fileId })
+        .then(async file => {
+            if (file.user_id.toString() != req.user._id.toString()) res.status(400).json({ error: "File doesnot exist" });
+
+            if (req.file.originalname != file.file_name) {
+                let result = await cloudinary.uploader.upload(req.file.path, { resource_type: AUTO, quality: AUTO });
+                file.cloudinary_id = result.secure_url;
+                file.file_name = req.file.originalname;
             }
+            file.title = title;
+            file.description = description;
+            file.save()
+                .then(result => {
+                    if (result) {
+                        return res.status(200).json({ message: "Successfully edited", result });
+                    } else {
+                        return res.status(400).json({ error: "err" });
+                    }
+                })
+                .catch(err => res.status(400).json({ error: err }))
         })
-        .catch(err => res.status(400).json({ error: err }))     
-    })
-    .catch(() => res.status(400).json({ error: "File doesnot exist" }))  
+        .catch(() => res.status(400).json({ error: "File doesnot exist" }))
 })
 
 // create short url
 //  Return - message, data
 
-router.post("/file/shorturl/:FileId", requireLogin, (req, res) => {
+router.post("/file/shorturl/:fileId", requireLogin, (req, res) => {
     const baseUrl = "http:localhost:5000/user/file/shorturl";
     const urlCode = shortid.generate();
     const shortUrl = baseUrl + "/" + urlCode;
-
-    File.findByIdAndUpdate(req.params.FileId, {$set:{url_code:urlCode, short_url:shortUrl}}, {upsert:true})
-    .then(file => {
-        return res.status(201).json({message: "Created sucessfuly", data: {shortUrl}});
-    })
-    .catch(err => res.status(400).json({ error: "File doesnot exist" }))
+    File
+        .findById({ _id: req.params.fileId })
+        .then(async file => {
+            if (file.user_id.toString() != req.user._id.toString()) res.status(400).json({ error: "File doesnot exist" })
+            file.short_url = shortUrl;
+            file.url_code = urlCode
+            await file.save();
+            return res.status(201).json({ message: "Created sucessfuly", data: { shortUrl } });
+        })
+        .catch(err => res.status(400).json({ error: "File doesnot exist" }))
 })
 
 // Redirect short url
@@ -212,11 +218,11 @@ router.post("/file/shorturl/:FileId", requireLogin, (req, res) => {
 
 router.get("/file/shorturl/:urlCode", (req, res) => {
     const urlCode = req.params.urlCode;
-    File.findOne({url_code:urlCode})
-    .then(file => {
-        return res.status(200).redirect(file.cloudinary_id);
-    })
-    .catch(err => res.status(400).json({ error: "File doesnot exist" }))
+    File.findOne({ url_code: urlCode })
+        .then(file => {
+            return res.status(200).redirect(file.cloudinary_id);
+        })
+        .catch(err => res.status(400).json({ error: "File doesnot exist" }))
 })
 
 
